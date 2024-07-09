3e044972c71997af24894d3ffe91d8561f9569ec
DemoProjectadding <srinivasoptimist7@gmail.com>
2024-07-09 19:09:56 +0530

    added parameters

---
+++ src/test/java/com/tests/LoginTest.java
@@ -0,0 +1,37 @@
+package com.tests;
+
+import com.utilities.*;
+import java.io.IOException;
+
+import org.testng.Reporter;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.pages.BaseClass;
+import com.pages.HomePage;
+import com.pages.LoginPage;
+
+public class LoginTest extends BaseClass {
+	@Test(dataProvider="getData")
+	public void loginuser(String method, String emailid,String password) throws IOException, InterruptedException {
+		HomePage homepage = new HomePage(driver);
+		LoginPage loginpage = new LoginPage(driver);
+		System.out.println(method);
+		
+		homepage.clicksignin();
+		Reporter.log("clicked signin", true);
+		loginpage.enteremail(emailid);
+		loginpage.clickcontinue();
+		Thread.sleep(5000);
+		loginpage.enterpassword(password);
+		loginpage.clicklogin();
+		Thread.sleep(5000);
+	}
+
+	@DataProvider
+	public Object[][] getData() throws IOException {
+
+		Object data[][] = ExcelData.getData("amazon_credentials");
+		return data;
+	}
+}
---
+++ src/test/java/com/tests/LoginTest2.java
@@ -0,0 +1,34 @@
+package com.tests;
+
+import java.io.IOException;
+
+import org.testng.Reporter;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.pages.BaseClass;
+import com.pages.HomePage;
+import com.pages.LoginPage;
+
+public class LoginTest2 extends BaseClass {
+
+	
+	@Parameters({"Team"})
+	@Test
+	public void loginuser2(String Team) throws IOException, InterruptedException {
+		HomePage homepage = new HomePage(driver);
+		LoginPage loginpage=new LoginPage(driver);
+		homepage.clicksignin();
+		Reporter.log("clicked signin", true);
+	    loginpage.enteremail(getObject("emailid"));
+	    loginpage.clickcontinue();
+	    Thread.sleep(5000);
+	    loginpage.enterpassword(getObject("password"));
+	    loginpage.clicklogin();
+	    Thread.sleep(5000);
+	    System.out.println("Logged IN successful to amazon");
+	    System.out.println("My Team name is : "+Team);
+	    driver.close();
+	}
+
+}
